1e37b2407ec13f9618c3015319f1915dc834d1f7
fermintz <fermintz@gmail.com>
2021-12-06 11:45:30 +0900

    snackbar 변경

--- src/components/modal/snackbar.vue
+++ src/components/modal/snackbar.vue
@@ -1,5 +1,13 @@
 <template>
-  <v-snackbar
+  <div class="snackbar" v-if="visible">
+    <div class="inner">
+      <div class="text">일반 마사지샵 수건류 50L 상품을 담았습니다</div>
+      <div class="btns">
+        <v-btn text @click="visible = false">닫기</v-btn>
+      </div>
+    </div>
+  </div>
+  <!-- <v-snackbar
     v-model="visible"
     :timeout="timeout"
     rounded="pill"
@@ -9,7 +17,7 @@
       mdi-basket-plus-outline
     </v-icon>
     {{ text }}
-  </v-snackbar>
+  </v-snackbar> -->
 </template>
 
 <script>
@@ -21,7 +29,15 @@ export default {
   data(){
     return{
       visible:false,
-      timeout:1000,
+    }
+  },
+  watch:{
+    visible(value){
+      if(value === true){
+        setTimeout(()=>{
+          this.visible = false
+        }, 500)
+      }
     }
   },
   methods:{
@@ -34,9 +50,57 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.v-snackbar{
-  text-align:center;
+.snackbar{
+  display:flex;
+  align-items: center;
+  justify-content: center;
+  position: fixed;
+  width:100%;
+  top:50%;
+  left:50%;
+  padding:0 40px;
+  z-index:99;
+  transform: translate(-50% ,-50%);
+  animation:upSlide 0.25s forwards;
 
-  .v-icon{margin-right:10px;}
+  @keyframes upSlide {
+    0%{
+      top:53%;
+
+    }
+    50%{
+      top:50%;
+ 
+    }
+  }
+
+  .inner{
+    width:auto;
+    text-align:center;
+    overflow:hidden;
+    border-radius:5px;
+    background:rgba(0,0,0,0.7);
+    padding:8px;
+   
+    .text{
+      color:#fff;
+      display:inline-block;
+      padding:5px;
+      font-size:12px; 
+    }
+    .btns{
+      display:none;
+      text-align:center;
+      .v-btn{
+        min-width:auto;
+        padding:0px 20px;
+        color:#00FFE5;
+        height:26px;
+        font-size:12px;
+        border-radius:13px;
+      }
+    }
+    
+  }
 }
 </style>
--- src/views/MyOrderDetail.vue
+++ src/views/MyOrderDetail.vue
@@ -35,28 +35,37 @@
           <dd>배송중</dd>
         </dl>
       </div>
- 
-      <div class="btns">
-        <v-btn text @click="$router.push('workHistory')">작업히스토리</v-btn>
-        <v-btn text @click="$refs.orderCancle.handle(true)">주문취소</v-btn>
-      </div>
 
-      
+      <div class="btns">
+        <v-btn text @click="$router.push('workHistory')">
+          <label>작업히스토리</label>
+          <v-icon>mdi-chevron-right</v-icon>
+        </v-btn>
+        <v-btn text @click="$refs.orderCancle.handle(true)">
+          <label>주문취소</label>
+          <v-icon>mdi-close-circle</v-icon>
+        </v-btn>
+      </div>
     </div>
 
     <div class="line" />
 
     <div class="order_info_tabs">
       <div class="tabs">
-        <div 
-          class="btn" 
-          v-ripple 
-          v-for="(item, index) in ['예약정보','상품정보','결제정보','추가결제']" 
+        <div
+          class="btn"
+          v-ripple
+          v-for="(item, index) in [
+            '예약정보',
+            '상품정보',
+            '결제정보',
+            '추가결제',
+          ]"
           :key="index"
           @click="tabActive = index"
-          :class="{active:tabActive === index}"
+          :class="{ active: tabActive === index }"
         >
-          {{item}}
+          {{ item }}
         </div>
       </div>
 
@@ -76,20 +85,20 @@
 
           <div class="order-info">
             <h3>예약자</h3>
-            
+
             <div class="info_box">
               <span class="name">박수민</span>
               <span class="phone">
-                <v-icon>mdi-phone</v-icon> 
+                <v-icon>mdi-phone</v-icon>
                 <label>010-1234-1234</label>
               </span>
               <span class="address">
-                <v-icon>mdi-map-marker</v-icon> 
+                <v-icon>mdi-map-marker</v-icon>
                 <label>부산시 북구 구남언덕로 15 3층</label>
               </span>
               <span class="gate">
-                <v-icon>mdi-lock</v-icon> 
-                <label>#0456 +  경비버튼</label>
+                <v-icon>mdi-lock</v-icon>
+                <label>#0456 + 경비버튼</label>
               </span>
             </div>
           </div>
@@ -101,12 +110,14 @@
             <div class="info_box">
               <span class="name">세탁프리워시 금정점</span>
               <span class="phone">
-                <v-icon>mdi-phone</v-icon> 
+                <v-icon>mdi-phone</v-icon>
                 <label>010-8525-4561</label>
               </span>
               <span class="address">
-                <v-icon>mdi-map-marker</v-icon> 
-                <label>부산시 금정구 부산대학로 63번길 2 과학기술연구동 201호</label>
+                <v-icon>mdi-map-marker</v-icon>
+                <label
+                  >부산시 금정구 부산대학로 63번길 2 과학기술연구동 201호</label
+                >
               </span>
             </div>
           </div>
@@ -120,14 +131,11 @@
               <dd>
                 <ul>
                   <li>
-                    오염 제거요청(혈액/음식물/토사물/기타 등) 오염 제거요청(혈액/음식물/토사물/기타 등)
+                    오염 제거요청(혈액/음식물/토사물/기타 등) 오염
+                    제거요청(혈액/음식물/토사물/기타 등)
                   </li>
-                  <li>
-                    수선 요청
-                  </li>
-                  <li>
-                    바지 기장 접은 만큼 줄여주세요!
-                  </li>
+                  <li>수선 요청</li>
+                  <li>바지 기장 접은 만큼 줄여주세요!</li>
                 </ul>
               </dd>
             </dl>
@@ -137,8 +145,10 @@
             </dl>
           </div>
         </div>
-      </div> <!-- inner -->
-    </div> <!-- tab_contents -->
+      </div>
+      <!-- inner -->
+    </div>
+    <!-- tab_contents -->
 
     <div class="tab_contents" v-show="tabActive === 1">
       <div class="inner">
@@ -152,12 +162,8 @@
                 <strong>3,000원</strong>
               </div>
             </div>
-            <div class="amount">
-              3
-            </div>
-            <div class="total">
-              12,000 원
-            </div>
+            <div class="amount">3</div>
+            <div class="total">12,000 원</div>
           </div>
           <div class="item">
             <div class="left">
@@ -167,12 +173,8 @@
                 <strong>3,000원</strong>
               </div>
             </div>
-            <div class="amount">
-              3
-            </div>
-            <div class="total">
-              12,000 원
-            </div>
+            <div class="amount">3</div>
+            <div class="total">12,000 원</div>
           </div>
         </div>
 
@@ -186,16 +188,14 @@
                 <strong>3,000원</strong>
               </div>
             </div>
-            <div class="amount">
-              2
-            </div>
-            <div class="total">
-              8,000 원
-            </div>
-          </div> <!-- item -->
+            <div class="amount">2</div>
+            <div class="total">8,000 원</div>
+          </div>
+          <!-- item -->
         </div>
       </div>
-    </div> <!-- tab_contents -->
+    </div>
+    <!-- tab_contents -->
 
     <div class="tab_contents" v-show="tabActive === 2">
       <div class="inner">
@@ -256,7 +256,8 @@
           </ul>
         </div>
       </div>
-    </div> <!-- tab_contents -->
+    </div>
+    <!-- tab_contents -->
 
     <div class="tab_contents" v-show="tabActive === 3">
       <div class="inner">
@@ -289,126 +290,125 @@
       </div>
     </div>
 
-    <OrderCancle ref="orderCancle"/>
-  </div> <!-- myOrderDetail -->
+    <OrderCancle ref="orderCancle" />
+  </div>
+  <!-- myOrderDetail -->
 </template>
 
 <script>
-import OrderCancle from '@/components/modal/orderCancle'
+import OrderCancle from "@/components/modal/orderCancle";
 
 export default {
-  components:{
+  components: {
     OrderCancle,
   },
-  data(){
-    return{
-      tabActive:0,
-      timelineActive:0,
-    }
+  data() {
+    return {
+      tabActive: 0,
+      timelineActive: 0,
+    };
   },
-  methods: {
-    
-  },
+  methods: {},
 };
 </script>
 
 <style lang="scss" scoped>
 .myOrderDetail {
-  padding-bottom:70px;
+  padding-bottom: 70px;
 
-  ul{
-    padding:0px;
+  ul {
+    padding: 0px;
   }
 
-  .inner{
-    padding:20px;
+  .inner {
+    padding: 20px;
   }
 
-  .order_info_tabs{
-    .tabs{
-      display:flex;
+  .order_info_tabs {
+    .tabs {
+      display: flex;
       align-items: center;
 
-      .btn{
-        display:flex;
+      .btn {
+        display: flex;
         align-items: center;
         justify-content: center;
-        flex:1;
-        height:50px;
-        border-bottom:2px solid #e2e2e2;
+        flex: 1;
+        height: 50px;
+        border-bottom: 2px solid #e2e2e2;
       }
-      .btn.active{
-        color:#0CA0E2;
-        border-bottom:2px solid #0CA0E2
+      .btn.active {
+        color: #0ca0e2;
+        border-bottom: 2px solid #0ca0e2;
       }
     }
   }
 
-  .line{
-    height:10px;
-    background:#f2f2f2;
+  .line {
+    height: 10px;
+    background: #f2f2f2;
   }
 
   .order_state {
-    padding:20px;
+    padding: 20px;
 
-    .state_title{
-      width:100%;
-      text-align:center;
-      span{
-
+    .state_title {
+      width: 100%;
+      text-align: center;
+      span {
       }
-      h2{
-        font-size:24px;
-        font-weight:bold;
+      h2 {
+        font-size: 24px;
+        font-weight: bold;
       }
     }
-    
-    .state_timeline{
-      display:flex;
+
+    .state_timeline {
+      display: flex;
       align-items: flex-start;
       justify-content: center;
-      margin-top:30px;
-      border-radius:10px;
+      margin-top: 30px;
+      border-radius: 10px;
 
-      dl{
-        margin:0 10px;
-        display:flex;
+      dl {
+        margin: 0 10px;
+        display: flex;
         flex-direction: column;
         align-items: center;
-        text-align:center;
-        
-        dt{
-          display:flex;
+        text-align: center;
+
+        dt {
+          display: flex;
           justify-content: center;
           align-items: center;
-          width:50px;
-          height:50px;
-          border-radius:20px;
-          background:#f2f2f2;
+          width: 50px;
+          height: 50px;
+          border-radius: 25px;
+          background: #f6f6f6;
 
-          .v-icon{
-            font-size:22px;
-            color:#c2c2c2;
+          .v-icon {
+            font-size: 22px;
+            color: #d2d2d2;
           }
         }
-        dd{
-          margin-top:8px;
-          color:#aaa
+        dd {
+          margin-top: 8px;
+          font-size:12px;
+          color: #aaa;
         }
       }
 
-      dl.active{
-        dt{
-          background:#fff;
-          border:2px solid #0CA0E2;
-          .v-icon{
-            color:#0CA0E2;
+      dl.active {
+        dt {
+          background: #fff;
+          border: 2px solid #0ca0e2;
+          .v-icon {
+            color: #0ca0e2;
           }
         }
-        dd{
-          color:#292929;
-          font-weight:bold;
+        dd {
+          color: #292929;
+          font-weight: bold;
         }
       }
     }
@@ -420,18 +420,24 @@ export default {
       .v-btn {
         height: 30px;
         min-height: 30px;
-        margin-left:10px;
-        border-radius:15px;
-        background:#f2f2f2;
+        margin-left: 10px;
+        border-radius: 15px;
+        padding: 0 10px;
+        background: #f2f3f8;
+
+        label {
+        }
+        .v-icon {
+          font-size: 14px;
+          margin-left: 5px;
+        }
       }
 
-      .v-btn:last-child{
-        background:#FBEFF4;
-        color:#f11064;
+      .v-btn:last-child {
+        background: #fbeff4;
+        color: #f11064;
       }
     }
-
-    
   }
 
   .divider {
@@ -444,74 +450,72 @@ export default {
     margin-bottom: 20px;
   }
 
-  .order_number{
-    display:flex;
+  .order_number {
+    display: flex;
     align-items: center;
-    border-radius:5px;
-    margin-bottom:10px;
-    span{
-      width:80px;
+    border-radius: 5px;
+    margin-bottom: 10px;
+    span {
+      width: 80px;
     }
-    strong{
-      flex:1;
-      font-weight:bold;
-      font-size:14px;
+    strong {
+      flex: 1;
+      font-weight: bold;
+      font-size: 14px;
     }
-    .v-btn{
-      min-width:auto;
-      height:24px;
-      background:#e2e2e2;
-      padding:0 10px;
-      color:#292929;
-      font-size:11px;
+    .v-btn {
+      min-width: auto;
+      height: 24px;
+      background: #e2e2e2;
+      padding: 0 10px;
+      color: #292929;
+      font-size: 11px;
     }
   }
 
-  .order-date{
-    display:flex;
+  .order-date {
+    display: flex;
     align-items: center;
-    border-radius:5px;
+    border-radius: 5px;
 
-    span{
-      width:80px;
-      color:#292929;
+    span {
+      width: 80px;
+      color: #292929;
     }
 
-    strong{
-      color:#0CA0E2;
-      font-size:14px;
-      font-weight:bold;
-      border-radius:100px;
+    strong {
+      color: #0ca0e2;
+      font-size: 14px;
+      font-weight: bold;
+      border-radius: 100px;
     }
   }
 
   .order-info {
-    .info_box{
-      span{
-        display:block;
-        margin-bottom:5px;
-        background:#f8f8f8;
-        padding:10px;
-        border-radius:5px;
+    .info_box {
+      span {
+        display: block;
+        margin-bottom: 5px;
+        background: #f8f8f8;
+        padding: 10px;
+        border-radius: 5px;
 
-        .v-icon{
-          font-size:14px;
-          margin-right:10px;
-          margin-top:2px;
-          color:#aaa
+        .v-icon {
+          font-size: 14px;
+          margin-right: 10px;
+          margin-top: 2px;
+          color: #aaa;
         }
       }
-      span:last-child{
-        margin-bottom:0px;
+      span:last-child {
+        margin-bottom: 0px;
       }
 
-
-      span.address, span.phone{
-        display:flex;
+      span.address,
+      span.phone {
+        display: flex;
         align-items: flex-start;
-        line-height:1.4;
-
-        
+        line-height: 1.4;
       }
     }
   }
@@ -522,7 +526,7 @@ export default {
 
       dt {
         font-size: 12px;
-        margin-bottom:10px;
+        margin-bottom: 10px;
         color: #888;
 
         .v-icon {
@@ -536,13 +540,13 @@ export default {
       dd {
         ul {
           li {
-            background:#F1F5F9;
-            margin-bottom:10px;
-            padding:10px;
-            border-radius:5px;
+            background: #f1f5f9;
+            margin-bottom: 10px;
+            padding: 10px;
+            border-radius: 5px;
           }
-          li:last-child{
-            margin-bottom:0px;
+          li:last-child {
+            margin-bottom: 0px;
           }
         }
       }
@@ -550,8 +554,8 @@ export default {
     dl.delivery {
       dd {
         background: #f6f3ea;
-        padding:10px;
-        border-radius:5px;
+        padding: 10px;
+        border-radius: 5px;
       }
     }
 
@@ -560,88 +564,92 @@ export default {
     }
   }
 
-  .user_product{
-    h3{
-      margin-bottom:10px;
+  .user_product {
+    h3 {
+      margin-bottom: 10px;
     }
-    .item{
-      display:flex;
+    .item {
+      display: flex;
       align-items: center;
-      border:1px solid #e2e2e2;
-      padding:15px;
-      border-radius:5px;
-      margin-bottom:10px;
+      border: 1px solid #e2e2e2;
+      padding: 15px;
+      border-radius: 5px;
+      margin-bottom: 10px;
 
-      .left{
-        flex:1;
-        label{
-          display:block;
+      .left {
+        flex: 1;
+        label {
+          display: block;
         }
-        .price{
-          display:flex;
+        .price {
+          display: flex;
           align-items: center;
-          font-size:12px;
-          span{
+          font-size: 12px;
+          span {
             text-decoration: line-through;
-            color:#999;
+            color: #999;
           }
-          strong{
-            margin-left:5px;
+          strong {
+            margin-left: 5px;
           }
         }
       }
 
-      .amount{
-        display:flex;
+      .amount {
+        display: flex;
         justify-content: center;
         align-items: center;
-        border-radius:18px;
-        width:30px;
-        height:30px;
-        font-size:12px;
-        background:#f2f2f2;
+        border-radius: 18px;
+        width: 30px;
+        height: 30px;
+        font-size: 12px;
+        background: #f2f2f2;
       }
-      .total{
-        width:90px;
-        text-align:right;
+      .total {
+        width: 90px;
+        text-align: right;
       }
     }
 
-    .price_total{
-      background:#f2f2f2;
-      border-radius:5px;
-      padding:15px;
-      display:flex;
+    .price_total {
+      background: #f2f2f2;
+      border-radius: 5px;
+      padding: 15px;
+      display: flex;
       align-items: center;
       justify-content: space-between;
-      span{font-size:12px;}
-      strong{font-weight:bold;}
+      span {
+        font-size: 12px;
+      }
+      strong {
+        font-weight: bold;
+      }
     }
   }
 
-  .user_product.check{
-    margin-top:30px;
+  .user_product.check {
+    margin-top: 30px;
 
-    .item{
-      background:#FBEFF4;
-      border:0px;
-      .amount{
-        color:#f11064;
-        background:#fff;
+    .item {
+      background: #fbeff4;
+      border: 0px;
+      .amount {
+        color: #f11064;
+        background: #fff;
       }
-      .total{
-        color:#f11064;
+      .total {
+        color: #f11064;
       }
     }
   }
 
   .add-payment-list {
     ul {
-      padding:0px;
+      padding: 0px;
       li {
         border: 1px solid #e2e2e2;
         border-radius: 5px;
-        overflow:hidden;
+        overflow: hidden;
 
         .top {
           padding: 15px;
@@ -653,7 +661,7 @@ export default {
             .date {
               color: #888;
               font-size: 12px;
-              margin-left:10px;
+              margin-left: 10px;
             }
             .text {
               font-size: 12px;
@@ -665,23 +673,23 @@ export default {
             display: flex;
             justify-content: flex-end;
             align-items: center;
-            margin-top:20px;
+            margin-top: 20px;
 
             label {
               color: #888;
               font-size: 12px;
-              margin-right:30px;
+              margin-right: 30px;
             }
-            span{
-              display:flex;
+            span {
+              display: flex;
               align-items: center;
-              font-weight:bold;
-              line-height:1;
-              font-size:16px
+              font-weight: bold;
+              line-height: 1;
+              font-size: 16px;
             }
             span:after {
               content: "원";
-              font-size:12px;
+              font-size: 12px;
               margin-left: 5px;
             }
           }
@@ -697,14 +705,14 @@ export default {
           align-items: center;
           overflow: hidden;
           border-top: 1px solid #e2e2e2;
-     
+
           .v-btn {
             justify-content: flex-end;
             align-items: center;
             width: 100%;
             border: 0px;
             border-radius: 0px;
-            min-height:40px;
+            min-height: 40px;
 
             label {
               margin-right: 10px;
@@ -723,7 +731,7 @@ export default {
     ul {
       li {
         padding: 15px;
-        background: #FBEFF4;
+        background: #fbeff4;
         border-radius: 5px;
         margin-bottom: 10px;
 
@@ -737,8 +745,8 @@ export default {
           align-items: center;
           justify-content: space-between;
           margin-top: 10px;
-          .price{
-            color:#f11064;
+          .price {
+            color: #f11064;
           }
 
           .price:after {
@@ -755,10 +763,10 @@ export default {
     }
 
     .breakdown {
-      .list{
+      .list {
         padding: 15px;
         border-radius: 5px;
-        background:#f8f8f8;
+        background: #f8f8f8;
 
         dl {
           align-items: center;
@@ -768,7 +776,7 @@ export default {
 
           dt {
             font-size: 12px;
-            color:#888;
+            color: #888;
           }
 
           dd:after {
@@ -780,7 +788,6 @@ export default {
           margin-bottom: 0px;
         }
       }
-      
     }
 
     .total {
@@ -793,16 +800,16 @@ export default {
         margin-right: 20px;
       }
       span {
-        display:flex;
+        display: flex;
         align-items: center;
         font-size: 18px;
-        font-weight:bold;
+        font-weight: bold;
       }
       span:after {
         content: "원";
         font-size: 14px;
         margin-left: 5px;
-        font-weight:normal
+        font-weight: normal;
       }
     }
   }
