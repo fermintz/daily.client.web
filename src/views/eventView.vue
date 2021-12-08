4b9a7ca5a616e6359095d5edd9fd8a4b59ee97ac
fermintz <fermintz@gmail.com>
2021-12-08 17:22:52 +0900

    coupon 주의사항 추가

--- src/views/eventView.vue
+++ src/views/eventView.vue
@@ -24,10 +24,16 @@
     </div>
 
     <div class="guide">
-      <h4>쿠폰 주의사항</h4>
+      <h4>
+        <v-icon>mdi-information-outline</v-icon>
+        <label>쿠폰 주의사항</label>
+      </h4>
       <ul>
-        <li>- 쿠폰은 중복사용이 불가능합니다</li>
-        <li>- 다운로드는 1회로 제한합니다</li>
+        <li>할인쿠폰은 발행대상, 발행경로, 사용대상 등에 따라 구분될 수 있으며, 할인쿠폰의 세부구분, 할인금액(할인율), 사용방법, 사용기한 및 제한에 대한 사항은 할인쿠폰 또는 서비스화면에 표시됩니다. 할인쿠폰의 종류 및 내용과 발급여부에 관하여는 데일리세탁의 정책에 따라 달라질 수 있습니다.</li>
+        <li>할인쿠폰은 현금으로 출금될 수 없으며, 할인쿠폰에 표시된 유효기간이 만료되거나 이용계약이 종료되면 소멸합니다.</li>
+        <li>할인쿠폰은 데일리세탁에서 별도로 명시한 경우를 제외하고는 타인에게 양도할 수 없으며, 부정한 목적이나 용도로 사용할 수 없습니다. 이를 위반한 경우에는 데일리세탁에서 할인쿠폰을 소멸시키거나 "회원"자격을 정지할 수 있습니다.</li>
+        <li>"회원"이 부정한 방법으로 할인쿠폰을 획득한 사실이 확인될 경우 소유 중인 할인쿠폰 회수, ID 삭제 및 형사고발 등 기타 조치를 취할 수 있습니다.</li>
+        <li>사용자가 직접 쿠폰에 명시된 내용 및 개별 주의사항(이용조건)을 확인하여야 할 책임이 있으며, 명시된 이용조건에 따라서만 쿠폰을 사용하실 수 있습니다.</li>
       </ul>
     </div>
   </div>
@@ -167,16 +173,29 @@ export default {};
     border-top:1px solid #e2e2e2;
 
     h4{
-      font-size:16px;
-      font-weight:bold;
+      display:flex;
+      align-items: center;
+      
+      .v-icon{
+        font-size:20px;
+        margin-right:5px;
+      }      
+
+      label{
+        font-size:16px;
+        font-weight:bold;
+      }
     }
     ul{
       padding:0px;
       margin-top:10px;
 
       li{ 
-        color:#898989;
-        margin-bottom:5px;
+        color:#292929;
+        margin-bottom:15px;
+        background:#f2f2f2;
+        padding:10px;
+        border-radius:4px;
       }
     }
   }
--- src/views/myCouponList.vue
+++ src/views/myCouponList.vue
@@ -16,13 +16,14 @@
     <dl class="coupon-guide">
       <dt>
         <v-icon>mdi-information-outline</v-icon>
-        <label>쿠폰 사용 안내</label>
+        <label>쿠폰 이용안내 및 주의사항</label>
       </dt>
       <dd>
-        <span>주문 1건에 1개의 쿠폰을 사용하실 수 있습니다.</span>  
-        <span>결제 시 쿠폰을 적용하지 않으면 할인이 적용되지 않습니다.</span>  
-        <span>유효기간이 경과된 쿠폰은 내역에서 자동 삭제됩니다.</span>  
-        <span>반품시 할인 쿠폰이 적용된 상품의 경우 할인된 금액은 환불되지 않습니다.</span>  
+        <span>할인쿠폰은 발행대상, 발행경로, 사용대상 등에 따라 구분될 수 있으며, 할인쿠폰의 세부구분, 할인금액(할인율), 사용방법, 사용기한 및 제한에 대한 사항은 할인쿠폰 또는 서비스화면에 표시됩니다. 할인쿠폰의 종류 및 내용과 발급여부에 관하여는 데일리세탁의 정책에 따라 달라질 수 있습니다.</span>  
+        <span>할인쿠폰은 현금으로 출금될 수 없으며, 할인쿠폰에 표시된 유효기간이 만료되거나 이용계약이 종료되면 소멸합니다.</span>  
+        <span>할인쿠폰은 데일리세탁에서 별도로 명시한 경우를 제외하고는 타인에게 양도할 수 없으며, 부정한 목적이나 용도로 사용할 수 없습니다. 이를 위반한 경우에는 데일리세탁에서 할인쿠폰을 소멸시키거나 "회원"자격을 정지할 수 있습니다.</span>  
+        <span>"회원"이 부정한 방법으로 할인쿠폰을 획득한 사실이 확인될 경우 소유 중인 할인쿠폰 회수, ID 삭제 및 형사고발 등 기타 조치를 취할 수 있습니다.</span>  
+        <span>사용자가 직접 쿠폰에 명시된 내용 및 개별 주의사항(이용조건)을 확인하여야 할 책임이 있으며, 명시된 이용조건에 따라서만 쿠폰을 사용하실 수 있습니다.</span>  
       </dd>
     </dl>
    
@@ -76,35 +77,31 @@ export default {
  
   
   .coupon-guide{
-    padding:15px;
-    background:#f8f8f8;
-    border-radius:10px;
+    border-top:1px solid #e2e2e2;
+    padding-top:30px;
 
     dt{
-      margin-bottom:10px;
+      display:flex;
+      align-items: center;
+      margin-bottom:15px;
+
       .v-icon{
-        font-size:18px;
+        font-size:20px;
         margin-right:5px;
       }
       label{
         font-weight:bold;
+        font-size:16px;
       }
     }
     dd{
       span{
-        display:flex;
-        align-items: flex-start;
-        margin-bottom:5px;
-        font-size:11px;
-      }
-      span:before{
-        content:'';
-        display: inline-block;
-        width:2px;
-        height:2px;
-        margin-right:5px;
-        margin-top:5px;
-        background:#292929;
+        display:block;
+        border-radius:4px;
+        padding:10px;
+        margin-bottom:15px;
+        font-size:12px;
+        background:#f2f2f2;
       }
     }
   }
