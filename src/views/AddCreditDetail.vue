1e54e1b9a68c68edafc7b266cd22dd4a98f827c9
fermintz <fermintz@gmail.com>
2022-03-29 17:05:31 +0900

    추가결제페이지 변경

--- src/views/AddCreditDetail.vue
+++ src/views/AddCreditDetail.vue
@@ -29,12 +29,59 @@
 
     <div class="divider-8"/>
 
+    <div class="credit">
+      <h3>결제방법 선택</h3>
+
+      <div class="credit-btns">
+        <div
+          class="credit-btn"
+          text
+          v-ripple
+          :class="{active:creditSeleted === index}"
+          v-for="(item, index) in credit"
+          :key="index"
+          @click="creditSeleted = index"
+        >
+          <div class="check"></div>
+          <div class="img" v-show="item.url">
+            <img :src="item.url" />
+          </div>
+          <div class="text">{{item.name}}</div>
+        </div>
+      </div>
+    </div>
+
     <div class="finish">
-      <v-btn text>결제하기</v-btn>
+      <v-btn text>
+        <strong>5,000원</strong> 결제하기
+      </v-btn>
     </div>
     
   </div>
 </template>
+
+<script>
+export default {
+  data(){
+    return{
+      creditSeleted: 0,
+      credit: [
+        {
+          name: "네이버페이",
+          url: "img/naverPay.png",
+        },
+        {
+          name: "카카오페이",
+          url: "img/kakaoPay.png",
+        },
+        {
+          name: "신용카드",
+        },
+      ],
+    }
+  }
+}
+</script>
 
 <style lang="scss" scoped>
 .addCreditDetail{
@@ -108,6 +155,59 @@
           color:#292929;
           font-weight:normal;
           margin-left:5px;
+        }
+      }
+    }
+  }
+
+  .credit {
+    h3{
+      margin-bottom:20px;
+      font-weight:bold;
+    }
+
+    padding:0 20px;
+
+    .credit-btns {
+      .credit-btn {
+        width: 100%;
+        height: 50px;
+        margin-bottom: 10px;
+        border: 1px solid #d2d2d2;
+        border-radius: 5px;
+        display: flex;
+        align-items: center;
+        padding: 0 10px;
+
+        &:last-child {
+          margin-bottom: 0;
+        }
+        .check {
+          display: block;
+          width: 22px;
+          height: 22px;
+          border: 1px solid #a2a2a2;
+          border-radius: 11px;
+          margin-right: 15px;
+        }
+        .img {
+          width: 70px;
+          img {
+            display: block;
+            height: 20px;
+          }
+        }
+        .text {
+          flex: 1;
+          font-size: 14px;
+          font-weight: bold;
+        }
+      }
+
+      .credit-btn.active {
+        border-color: #0ca0e2;
+        .check {
+          border: 7px solid #0ca0e2;
         }
       }
     }
